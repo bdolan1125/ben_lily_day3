b90eae2f03fb3c4a9fd48236228a57d15bde5c9a
Lillian Burnside <lillian.burnside@gmail.com>
2020-01-08 15:19:11 -0600

    finished fizzbuzz

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,30 +1,24 @@
 'use strict';
 
 $(function fizzBuzz(num){
-  for(let i = 0; i <= num; i++){
-    if(num % 3 === 0 && num % 5 === 0){
-      $('<div class="fizz-buzz-item fizzbuzz"> <span>fizzbuzz</span> </div>');
+  for(let i = 1; i <= num; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      $('.js-results').append('<div class="fizz-buzz-item fizzbuzz"> <span>fizzbuzz</span> </div>');
     }
-    else if(num % 3 === 0){
-      $('<div class="fizz-buzz-item fizz"> <span>fizz</span> </div>');
+    else if(i % 3 === 0){
+      $('.js-results').append('<div class="fizz-buzz-item fizz"> <span>fizz</span> </div>');
     }
-    else if(num % 5 === 0){
-      $('<div class="fizz-buzz-item buzz"> <span>buzz</span> </div>');
+    else if(i % 5 === 0){
+      $('.js-results').append('<div class="fizz-buzz-item buzz"> <span>buzz</span> </div>');
     }
     else {
-      $('<div class="fizz-buzz-item"> <span> ${num} </span> </div>');
+      $('.js-results').append(`<div class="fizz-buzz-item"> <span> ${i} </span> </div>`);
     }
   }
   
-  $('form').on('click', '.button', (event) => {
-    event.preventeDefault();
-    let num = $(event.currentTarget).find('input').attr('type');
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    let num = $(event.currentTarget).find('input').val();
     fizzBuzz(num);
   });
 });
-
-// $('form').on('click', '.button', (event) => {
-//   event.preventeDefault();
-//   let num = $(event.currentTarget).find('input').attr('type');
-//   fizzBuzz(num);
-// })
