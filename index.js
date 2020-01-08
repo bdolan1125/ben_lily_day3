7ce8c8d3d2995aeb0404250e863bc75d531bcff6
Benjamin Dolan <benjamin@thinkful.com>
2020-01-08 13:01:31 -0700

    finished cat_carousel

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 
 $( 'main').on( 'click', '.thumbnail', (event) => {
    event.preventDefault();
-   console.log(event.currentTarget)
-    let thumbSrc = $(event.currentTarget).attr('src');
-    let thumbAlt =  $(event.currentTarget).attr('alt');
-   // const targetCat = $(event.currentTarget);
-    //const otherCat = $('thumbnail').not(targetCat);
-    //const pressedBool = $(targetCat).attr('aria-pressed') === 'true';
+    let thumbSrc = $(event.currentTarget).find('img').attr('src');
+    let thumbAlt =  $(event.currentTarget).find('img').attr('alt');
     $('.hero img').attr('src', thumbSrc);
     $('.hero img').attr('alt', thumbAlt);
-    //$('hero img').attr('src', thumbSrc).attr('aria-pressed', false);
-   // $('hero img').attr('alt', thumbAlt).attr('aria-pressed', false);
-    
 });
