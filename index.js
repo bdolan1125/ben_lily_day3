fb5b0b28684866f81229eaab9c31c5b47e361619
Benjamin Dolan <benjamin@thinkful.com>
2020-01-08 12:48:30 -0700

    adding a11y to cat carusel

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
-$( 'main').on( 'click', '.thumbnail img', (event) => {
+$( 'main').on( 'click', '.thumbnail', (event) => {
    event.preventDefault();
+   console.log(event.currentTarget)
     let thumbSrc = $(event.currentTarget).attr('src');
-    $('.hero img').attr('src', thumbSrc);
     let thumbAlt =  $(event.currentTarget).attr('alt');
+   // const targetCat = $(event.currentTarget);
+    //const otherCat = $('thumbnail').not(targetCat);
+    //const pressedBool = $(targetCat).attr('aria-pressed') === 'true';
+    $('.hero img').attr('src', thumbSrc);
     $('.hero img').attr('alt', thumbAlt);
-    $('hero img').attr('src', thumbSrc);
-    $('hero img').attr('alt', thumbAlt);
+    //$('hero img').attr('src', thumbSrc).attr('aria-pressed', false);
+   // $('hero img').attr('alt', thumbAlt).attr('aria-pressed', false);
+    
 });
